a602e9258063ec66628d6568076f576cef29c4a7
ZHANGXIANG <863413076@qq.com>
2017-11-15 16:43:24 +0800

    使用生成的Mybatis文件进行数据库操作(需要在maven build时进行resources设置，使生成的war包中包含mapper.xml文件,否则Invalid bound statement (not found))

--- src/main/java/com/zx/demo/controller/IndexController.java
+++ src/main/java/com/zx/demo/controller/IndexController.java
@@ -3,7 +3,9 @@ package com.zx.demo.controller;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.core.util.StatusPrinter;
 import com.zx.demo.dao.UserDao;
+import com.zx.demo.dao.mybatis.UserMapper;
 import com.zx.demo.domain.User;
+import com.zx.demo.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,9 @@ public class IndexController {
 
     @Autowired
     private UserDao UserDaoImpl;
+
+    @Autowired
+    private UserService userService;
 
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -53,8 +58,11 @@ public class IndexController {
     public String getDemo() {
         logger.info("demo");
         // print internal state
-        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
-        StatusPrinter.print(lc);
+       /* LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        StatusPrinter.print(lc);*/
+        logger.info("进行add");
+        userService.addUser();
+        logger.info("add over");
         return "demo";
     }
 
