d978012474fc19760b6ca82fd66ed8d2078a8a35
ZHANGXIANG <863413076@qq.com>
2017-11-16 13:49:11 +0800

    内部处理异常(不处理会被tomcat日志捕获,从而不输出到Output)

--- src/main/java/com/zx/demo/controller/IndexController.java
+++ src/main/java/com/zx/demo/controller/IndexController.java
@@ -1,9 +1,8 @@
 package com.zx.demo.controller;
 
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.core.util.StatusPrinter;
+//import ch.qos.logback.classic.LoggerContext;
+//import ch.qos.logback.core.util.StatusPrinter;
 import com.zx.demo.dao.UserDao;
-import com.zx.demo.dao.mybatis.UserMapper;
 import com.zx.demo.domain.User;
 import com.zx.demo.service.UserService;
 import org.slf4j.Logger;
@@ -13,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 /**
  * Created by ZX on 2017/11/13.
@@ -26,7 +26,7 @@ public class IndexController {
     @Autowired
     private UserService userService;
 
-    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+   protected final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
     @RequestMapping("/hello")
     public @ResponseBody
@@ -43,6 +43,7 @@ public class IndexController {
 
     @GetMapping("/index")
     public String getIndex() {
+        int a = 1/0;
         logger.info("进行add");
         User user = new User();
         user.setId("1");
@@ -56,13 +57,13 @@ public class IndexController {
 
     @RequestMapping("/demo")
     public String getDemo() {
-        logger.info("demo");
+              logger.info("demo");
         // print internal state
        /* LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
         StatusPrinter.print(lc);*/
-        logger.info("进行add");
+           logger.info("进行add");
         userService.addUser();
-        logger.info("add over");
+          logger.info("add over");
         return "demo";
     }
 
---
+++ src/main/java/com/zx/demo/exceptionHandler/MainExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.zx.demo.exceptionHandler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * Created by ZX on 2017/11/16.
+ */
+
+@ControllerAdvice
+public class MainExceptionHandler {
+
+    protected final Logger logger = LoggerFactory.getLogger(MainExceptionHandler.class);
+
+    @ExceptionHandler(value = {Exception.class})
+    public void test(Exception e){
+        logger.info("输出异常");
+        logger.error(e.toString());
+    }
+
+}
