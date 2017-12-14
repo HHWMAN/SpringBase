c3ae35c9032f900334099e504faad37becec0b65
ZHANGXIANG <863413076@qq.com>
2017-12-14 16:48:50 +0800

    自动注入注解添加

--- src/main/java/com/zx/demo/security/AppUserDetails.java
+++ src/main/java/com/zx/demo/security/AppUserDetails.java
@@ -1,6 +1,10 @@
 package com.zx.demo.security;
 
+import com.zx.demo.dao.mybatis.RoleMapper;
+import com.zx.demo.dao.mybatis.UserMapper;
 import com.zx.demo.domain.mybatis.User;
+import com.zx.demo.domain.mybatis.UserRoleExample;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -12,13 +16,28 @@ import java.util.Collection;
 
 @Component
 public class AppUserDetails implements UserDetails {
+
+	@Autowired
+	UserMapper userMapper;
+
+	@Autowired
+	RoleMapper roleMapper;
 	
 
 	private User appUser;   //对应数据库中的用户
 	private Collection<GrantedAuthority>  appUserAuthorities; //应用用户权限
 
+
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
+        //通过用户名查role
+        String userId = appUser.getUser_id();
+		UserRoleExample userRoleExample = new UserRoleExample();
+		userRoleExample.createCriteria().andUser_idEqualTo(userId);
+
+		//查询所有role对应的权限
+
+
 		return null;
 	}
 
@@ -53,4 +72,19 @@ public class AppUserDetails implements UserDetails {
 	}
 
 
+	public User getAppUser() {
+		return appUser;
+	}
+
+	public void setAppUser(User appUser) {
+		this.appUser = appUser;
+	}
+
+	public Collection<GrantedAuthority> getAppUserAuthorities() {
+		return appUserAuthorities;
+	}
+
+	public void setAppUserAuthorities(Collection<GrantedAuthority> appUserAuthorities) {
+		this.appUserAuthorities = appUserAuthorities;
+	}
 }
