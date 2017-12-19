3280990f431e72155ad4dbe960cbf8599548bb43
ZHANGXIANG <863413076@qq.com>
2017-12-19 10:27:57 +0800

    结构优化，userdetail作为纯粹实体类

--- src/main/java/com/zx/demo/security/AppUserDetails.java
+++ src/main/java/com/zx/demo/security/AppUserDetails.java
@@ -1,73 +1,27 @@
 package com.zx.demo.security;
 
-import com.zx.demo.dao.UserDao;
-import com.zx.demo.dao.impl.UserDaoImpl;
 
-import com.zx.demo.dao.mybatis.AuthorityMapper;
-import com.zx.demo.dao.mybatis.RoleAuthorityMapper;
-import com.zx.demo.dao.mybatis.UserRoleMapper;
 import com.zx.demo.domain.mybatis.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 
-
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 
-@Component
+
 public class AppUserDetails implements UserDetails {
 
-	@Autowired
-	UserDao userDaoImpl;
-
-	@Autowired
-	private UserRoleMapper userRoleMapper;
-
-	@Autowired
-	RoleAuthorityMapper roleAuthorityMapper;
-
-	@Autowired
-	AuthorityMapper authorityMapper;
-	
-
 	private User appUser;   //对应数据库中的用户
-	//private Collection<GrantedAuthority>  appUserAuthorities; //应用用户权限
 
+	private Collection<GrantedAuthority>  appUserAuthorities; //应用用户权限
+
+	public void setAppUserAuthorities(Collection<GrantedAuthority> appUserAuthorities) {
+		this.appUserAuthorities = appUserAuthorities;
+	}
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		List<AppUserAuthority> authorityList = new ArrayList<AppUserAuthority>();
-
-        //通过用户id查user、role对应
-        String userId = appUser.getUser_id();
-		UserRoleExample userRoleExample = new UserRoleExample();
-		userRoleExample.createCriteria().andUser_idEqualTo(userId);
-		List<UserRole> userRoleList = userRoleMapper.selectByExample(userRoleExample);
-
-		//处理得到的对应关系，获得role
-		List<Role> roleList = new ArrayList<Role>();
-		for(UserRole userRole:userRoleList){
-			String roleId = userRole.getRole_id();
-			//通过role,查询对应的权限id
-			RoleAuthorityExample roleAuthorityExample = new RoleAuthorityExample();
-			roleAuthorityExample.createCriteria().andRole_idEqualTo(roleId);
-			List<RoleAuthority> roleAuthorities =  roleAuthorityMapper.selectByExample(roleAuthorityExample);
-
-			for(RoleAuthority roleAuthority:roleAuthorities){
-
-				String authorityId = roleAuthority.getRauthority_id();
-				Authority authority = authorityMapper.selectByPrimaryKey(authorityId);
-				authorityList.add(new AppUserAuthority(authority));
-			}
-		}
-
-		return authorityList;
+		return appUserAuthorities;
 	}
 
 	@Override
@@ -109,40 +63,5 @@ public class AppUserDetails implements UserDetails {
 		this.appUser = appUser;
 	}
 
-	/*public Collection<GrantedAuthority> getAppUserAuthorities() {
-		return appUserAuthorities;
-	}*/
 
-
-	public UserDao getUserDaoImpl() {
-		return userDaoImpl;
-	}
-
-	public void setUserDaoImpl(UserDao userDaoImpl) {
-		this.userDaoImpl = userDaoImpl;
-	}
-
-	public UserRoleMapper getUserRoleMapper() {
-		return userRoleMapper;
-	}
-
-	public void setUserRoleMapper(UserRoleMapper userRoleMapper) {
-		this.userRoleMapper = userRoleMapper;
-	}
-
-	public RoleAuthorityMapper getRoleAuthorityMapper() {
-		return roleAuthorityMapper;
-	}
-
-	public void setRoleAuthorityMapper(RoleAuthorityMapper roleAuthorityMapper) {
-		this.roleAuthorityMapper = roleAuthorityMapper;
-	}
-
-	public AuthorityMapper getAuthorityMapper() {
-		return authorityMapper;
-	}
-
-	public void setAuthorityMapper(AuthorityMapper authorityMapper) {
-		this.authorityMapper = authorityMapper;
-	}
 }
--- src/main/java/com/zx/demo/security/AppUserDetailsService.java
+++ src/main/java/com/zx/demo/security/AppUserDetailsService.java
@@ -1,16 +1,21 @@
 package com.zx.demo.security;
 
 
+import com.zx.demo.dao.mybatis.AuthorityMapper;
+import com.zx.demo.dao.mybatis.RoleAuthorityMapper;
 import com.zx.demo.dao.mybatis.UserMapper;
-import com.zx.demo.domain.mybatis.User;
-import com.zx.demo.domain.mybatis.UserExample;
+import com.zx.demo.dao.mybatis.UserRoleMapper;
+import com.zx.demo.domain.mybatis.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 
@@ -20,7 +25,13 @@ public class AppUserDetailsService implements UserDetailsService {
 	UserMapper userMapper;
 
 	@Autowired
-	AppUserDetails appUserDetails;
+	private UserRoleMapper userRoleMapper;
+
+	@Autowired
+	RoleAuthorityMapper roleAuthorityMapper;
+
+	@Autowired
+	AuthorityMapper authorityMapper;
 
 
 	@Override
@@ -35,8 +46,37 @@ public class AppUserDetailsService implements UserDetailsService {
 		}
 
 		User user = userList.get(0);
+		AppUserDetails appUserDetails = new AppUserDetails();
 		appUserDetails.setAppUser(user);
-       // userDetails.getAuthorities();
+		appUserDetails.setAppUserAuthorities(getAuthort(user));
 		return appUserDetails;
+	}
+
+	public Collection<GrantedAuthority> getAuthort(User appUser){
+		Collection<GrantedAuthority> authorityList = new ArrayList<GrantedAuthority>();
+
+		//通过用户id查user、role对应
+		String userId = appUser.getUser_id();
+		UserRoleExample userRoleExample = new UserRoleExample();
+		userRoleExample.createCriteria().andUser_idEqualTo(userId);
+		List<UserRole> userRoleList = userRoleMapper.selectByExample(userRoleExample);
+
+		//处理得到的对应关系，获得role
+		List<Role> roleList = new ArrayList<Role>();
+		for(UserRole userRole:userRoleList){
+			String roleId = userRole.getRole_id();
+			//通过role,查询对应的权限id
+			RoleAuthorityExample roleAuthorityExample = new RoleAuthorityExample();
+			roleAuthorityExample.createCriteria().andRole_idEqualTo(roleId);
+			List<RoleAuthority> roleAuthorities =  roleAuthorityMapper.selectByExample(roleAuthorityExample);
+
+			for(RoleAuthority roleAuthority:roleAuthorities){
+
+				String authorityId = roleAuthority.getRauthority_id();
+				Authority authority = authorityMapper.selectByPrimaryKey(authorityId);
+				authorityList.add(new AppUserAuthority(authority));
+			}
+		}
+		return authorityList;
 	}
 }
