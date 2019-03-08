8d9a9b10e815c65783dfdee54ffc1ec21060e5bc
ZHANGXIANG <863413076@qq.com>
2019-03-08 14:37:49 +0800

    引入redis,测试本地连接

---
+++ src/test/jedis/TestPingRedis.java
@@ -0,0 +1,12 @@
+package jedis;
+
+import redis.clients.jedis.Jedis;
+
+public class TestPingRedis {
+    public static void main(String[] args) {
+        //连接本地的 Redis 服务
+        Jedis jedis = new Jedis("localhost",6379);
+        //查看服务是否运行
+        System.out.println("服务正在运行: "+jedis.ping());
+    }
+}
